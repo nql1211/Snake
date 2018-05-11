4b5c4ab8198a57ad6203ca1643963f0ef784da6d
nql1211 <37408306+nql1211@users.noreply.github.com>
2018-05-11 17:48:22 +0700

    Add files via upload

---
+++ Snake.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "console.h"
+
+
+enum trangThai { UP, DOWN, LEFT, RIGHT};
+
+
+struct toaDo
+{
+	int x, y;	// y là tung độ, x là hoành độ
+};
+
+struct Fruit
+{
+	toaDo td;
+};
+struct Snake
+{
+	toaDo dot[100];
+	int soDot;
+	trangThai tt;
+	int score;
+};
+
+void SetUp(Snake &snake, Fruit &fruit, int &score);
+void Draw(Snake snake, Fruit fruit, int score);
+void Input_Control(Snake &snake);
+void Logic(Snake &snake, Fruit &fruit, int &timeSleep, bool &gameOver, int &score);
